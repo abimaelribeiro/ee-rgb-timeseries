d71526f5bc71b989b5dbe763d8cc94407635e133
abimaelribeiro <abimaelribeiro@hotmail.com>
2023-06-19 09:26:21 -0400

    License update

--- landsat-timeseries-explorer.js
+++ landsat-timeseries-explorer.js
@@ -1,6 +1,7 @@
 /**
  * @license
  * Copyright 2021 Justin Braaten
+ * Copyright 2023 Abimael Ribeiro
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
